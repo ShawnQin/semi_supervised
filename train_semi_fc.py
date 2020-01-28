bf663fd76e02cb046135fcf1f1518af52d82d4bb
Shanshan Qin <qinss.pku@gmail.com>
2020-01-28 09:24:18 -0500

    add saved file

--- train_semi_fc.py
+++ train_semi_fc.py
@@ -234,7 +234,7 @@ if __name__=='__main__':
 	dirname='Results/' #CHANGE DIRECTORY CHOICE HERE, .save file will be stored in this directory
 	name = dirname + 'smep'
 	hpd1 = create_hyp_param_combination(hidden_sizes = [500],
-		n_epochs=20,
+		n_epochs=5,
 		batch_size=20,
 		n_it_neg=20,
 		n_it_pos=4,
@@ -247,7 +247,7 @@ if __name__=='__main__':
 		alphas_fwd= [np.float32(0.5), np.float32(0.375)], #0.5, 0.375
         alphas_unsuper=[np.float32(0.001),np.float32(0.0005)],  #Update for unsupervised
 		alphas_lat=[np.float32(0.01)], #CHANGE ALPHA LATERAL HERR
-        super_thr = np.float32(1),  # The ratio of superivsed samples, in the range of [0,1]
+        super_thr = np.float32(0.9),  # The ratio of superivsed samples, in the range of [0,1]
 	)
 	train_net_smep_mod(Network_SMEP_Mod(name, hpd1))
 	
